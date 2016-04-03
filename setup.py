9e337b4afd224ada5f84a4000166acbb1ebfb6dc
BuildTools <asydix@gmail.com>
2016-04-03 18:18:31 -0230

    Version Incrementation

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 def main():
     setup(
         name='simpleplugins',
-        version='0.1.0',
+        version='0.1.1',
         packages=[
             'simpleplugins'
         ],
